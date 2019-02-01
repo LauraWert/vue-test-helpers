17655611ed03bdc4ed1e1573edfec81966df00de
Rick <r.aikema@ultraware.nl>
2019-02-01 10:59:33 +0100

    Load transition stub with test utils config

--- src/create-test-app.ts
+++ src/create-test-app.ts
@@ -1,4 +1,4 @@
-import { createLocalVue } from '@vue/test-utils'
+import { config, createLocalVue } from '@vue/test-utils'
 import Quasar from 'quasar'
 import lang from 'quasar-framework/i18n/nl'
 import Vue, { VueConstructor } from 'vue'
@@ -24,7 +24,9 @@ export function createTestApp(options: ICreateTestAppOptions = {}): VueConstruct
   plugins.forEach((plugin: any) => plugin({ app: localVue, router, store, Vue }))
 
   // needs to be changed to config.stubs.transition if test-utils gets updated
-  localVue.component('transition', ExtendedTransitionStub)
+  // Vue.component('transition', ExtendedTransitionStub)
+  // @ts-ignore
+  config.stubs.transition = ExtendedTransitionStub
 
   return localVue
 }
