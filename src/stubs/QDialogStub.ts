d93417d6c48e84675cdc0e0f5f99c9e7ab43e3b4
Rick <r.aikema@ultraware.nl>
2019-09-02 13:12:47 +0200

    Add v-close-popup support

--- src/stubs/QDialogStub.ts
+++ src/stubs/QDialogStub.ts
@@ -7,11 +7,31 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 export class QDialogStub extends Vue {
   @Prop({ required: true, type: Boolean }) public value!: boolean
 
+  // tslint:disable-next-line:variable-name
+  public __hasPortal: boolean = true
+
+  get open(): boolean {
+    return this.value
+  }
+
+  set open(v: boolean) {
+    this.$emit('input', v)
+  }
+
   public render(h: CreateElement): VNode {
     return h('div', {
       style: {
-        display: (this.value ? 'block' : 'none'),
+        display: (this.open ? 'block' : 'none'),
       },
     }, this.$slots.default)
+  }
+
+  public beforeMount(): void {
+    // needs to be set for v-close-popup directive from quasar 1.1.x
+    this.__hasPortal = true
+  }
+
+  public hide(): void {
+    this.open = false
   }
 }
