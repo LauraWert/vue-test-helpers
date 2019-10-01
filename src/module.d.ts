90789f38003a698ff0060e3664b1650bf77082a3
Rick <r.aikema@ultraware.nl>
2019-10-01 10:52:22 +0200

    Add getValidationCtx unit  helper

--- src/module.d.ts
+++ src/module.d.ts
@@ -3,3 +3,4 @@ declare module 'quasar/lang/nl'
 declare module 'quasar/src/components/input'
 declare module 'quasar/src/components/select'
 declare module 'quasar/src/components/checkbox'
+declare const expect: Chai.ExpectStatic
